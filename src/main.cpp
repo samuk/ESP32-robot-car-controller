f88e1296cddc555bac10aa2b4f180256b7a96101
samuk <sam@bristolwireless.net>
2022-10-04 12:14:17 +0100

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -3,29 +3,79 @@
 #include <WiFiClientSecure.h>
 #include <AsyncTCP.h>
 #include <ESPAsyncWebServer.h>
-#include <Smartcar.h>
 #include "utility.h"
-
 #include "device/device.h"
 #include "device/device_web_server.h"
 #include "device/device_global_variables.h"
+#include <Wire.h>
+#include <SimpleFOC.h>
+#include "SimpleFOCDrivers.h"
+#include "comms/i2c/I2CCommanderMaster.h"
+
 
 void onWsEvent(AsyncWebSocket *server, AsyncWebSocketClient *client, AwsEventType type, void *arg, uint8_t *data, size_t len);
 
 AsyncWebServer ws_server(80);
 AsyncWebSocket ws("/ws");
 
+
+#define TARGET_I2C_ADDRESS 0x60
+#include <ESPmDNS.h>
+#include <Smartcar.h>
+#define I2C_SDA 21
+#define I2C_SCL 22
+#define I2C_Freq 100000UL
+
+
+// our RosmoESC's address...
+//#define TARGET_I2C_ADDRESS 0x60
+
+
+// global instance of commander - controller device version
+I2CCommanderMaster commander;
+
+void setup() {
+    // slow start - give RosmoESC a chance to boot up, serial to connect, etc...
+    delay(1000);
+
+{
+	 esp32_setup_peripherals();
+	 ws.onEvent(onWsEvent);
+	 ws_server.addHandler(&ws);
+ 	 esp32_setup_web_server(&ws_server);
+}
+
+
+ // this is a debug setup so initialize and wait for serial connection
+//Serial.begin(115200);
+//while (!Serial);
+
+
+ while (!Wire.begin(I2C_SDA, I2C_SCL, 100000ul))  {    // standard wire pins 
+       Serial.println("I2C failed to initialize");
+        delay(1000);
+    }
+    commander.addI2CMotors(TARGET_I2C_ADDRESS, 1); // only one motor in my test setup
+    commander.init();
+    Serial.println("I2C Commander intialized.");
+}
+
+
+//Example I2C commander command that needs to replace the UART stuff below
+//uint8_t status[4];
+//int numRead = commander.readRegister(0, REG_STATUS, status, 4); // 0 is the motor number
+
+//if (commander.writeRegister(0, REG_TARGET, &targetSpeed, 4)!=4) { // 0 is the motor number
+        // TODO handle I2C error
+//}
+
+
+
+//below from original
+
 uint8_t uart_message[3] = {97, 0, 0};
 
-void setup()
-{
-  esp32_setup_peripherals();
 
-  ws.onEvent(onWsEvent);
-  ws_server.addHandler(&ws);
-
-  esp32_setup_web_server(&ws_server);
-}
 
 void loop()
 {
